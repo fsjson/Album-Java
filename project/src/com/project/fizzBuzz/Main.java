7db933b7333cb9036481465fa62f6323e438c76c
Fachri <92247938+fsjson@users.noreply.github.com>
2023-04-21 04:52:02 +0700

    Change a little bit code program
    Change for loop conditional

--- project/src/com/project/fizzBuzz/Main.java
+++ project/src/com/project/fizzBuzz/Main.java
@@ -6,11 +6,11 @@ public class Main {
             if(n % 15 == 0) {
                 System.out.println("FizzBuzz");
             
-            } else if(n % 5 == 0) {
-                System.out.println("Buzz");
-            
             } else if(n % 3 == 0) {
                 System.out.println("Fizz");
+            
+            } else if(n % 5 == 0) {
+                System.out.println("Buzz");
             
             } else {
                 System.out.printf("\n%d\n",n);
